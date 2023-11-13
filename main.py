261a2cc751d033356d297fdf632c69440685eacf
Szymon Świędrych <43550433+S1NU5-P1@users.noreply.github.com>
2023-11-13 11:20:59 +0100

    Replace corrupted flight data with valid one and add help button

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 import numpy as np
 import smopy
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QMessageBox
 
 from flight_data import Flight
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
@@ -62,9 +61,6 @@ class Window(QtWidgets.QDialog):
         self.button = QtWidgets.QPushButton('Draw Map')
         self.button.clicked.connect(self.plot)
 
-        self.help_button = QtWidgets.QPushButton('Help')
-        self.help_button.clicked.connect(self.show_help)
-
         # Combobox
         self.combobox = QtWidgets.QComboBox()
         for filename in self.files:
@@ -78,7 +74,6 @@ class Window(QtWidgets.QDialog):
         layout.addWidget(self.slider_group)
         layout.addWidget(self.combobox)
         layout.addWidget(self.button)
-        layout.addWidget(self.help_button)
 
         self.setLayout(layout)
         self.resize(1200, 800)
@@ -137,9 +132,9 @@ class Window(QtWidgets.QDialog):
         msg_box = QMessageBox()
         msg_box.setIcon(QMessageBox.Question)
         msg_box.setText(
-            'Red - Original data\n'
-            'Yellow - Predicted\n'
-            'Green - Filtered using Kalman Filter\n')
+            'Red - Observation\n'
+            'Yellow - Prediction\n'
+            'Green - Position filtered using Kalman Filter\n')
         msg_box.exec_()
 
 
